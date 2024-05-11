3448440f239f7baa89bcd7e651bbb87c938468ac
Ana Maria <135929596+aninhamcr@users.noreply.github.com>
2024-05-11 11:27:25 -0300

    Alternative version hash.c

--- hash.c
+++ hash.c
@@ -1,21 +1,23 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+
+
 typedef struct {
-    char* chave;
-    int flag; // se 1 = ocupado, se 0 = vazio
+    char chave[16];
+    int flag; // se 1 = ocupado, se 0 = vazio, se 2 = deletada
 } Entry;
 
-int hash(char *string) { // hash function ; m = tamanho da tabela de dispersão 
-    int i, soma;
+
+int hash(char *string) { // m = tamanho da tabela de dispersão
+    int i = 0, soma = 0;
     for(i=0; string[i] != '\0';i++) {
-        soma += (string[i])*(i+1);
+        soma += (int)string[i]*(i+1);
     }
     soma = soma * 19;
     return abs(soma) % 101;
-}
+} 
 
 typedef struct {
     int tam; 
@@ -24,83 +26,137 @@ typedef struct {
 
 }Dictionary;
 
+Entry cria_entry(char *chave) {
+    Entry entrada;
+    strcpy(entrada.chave, chave);
+    entrada.flag = 1;
+
+    return entrada;
+}
+
 Dictionary* cria_dic(int tam) {
-    Dictionary *d;
+    Dictionary *d = (Dictionary*) malloc(sizeof(Dictionary)*101);
+    int i;
     d->tam = tam;
     d->q = 0;
     d->tabela = (Entry*) malloc(sizeof(Entry) * tam);
+    for(i=0;i<101;i++) {
+        d->tabela[i].flag = 0;
+    }
     return d;
 }
 
 int find(Dictionary *d, char *chave) {
     int index = hash(chave);
     int pos = index;
-    int j;
-    while(j<20 && d->tabela[pos].flag != 1 ) {
-        pos = (index + j*j + 23*j) % 101;
-    }
+    int j = 0;
 
-    if(d->tabela[pos].flag == 1) {
-        return 1;
+    if(d->tabela[pos].flag == 1 && strcmp(chave, d->tabela[pos].chave) == 0) {
+        return pos;
     }
     else {
-        return 0;
+        while(j<20) {
+            j++;
+            pos = (index + j*j + 23*j) % 101;
+            if(d->tabela[pos].flag == 1 && strcmp(chave, d->tabela[pos].chave) == 0) {
+                return pos;
+            }
+        }
+    
+        return -1;
+        
     }
 
 }
 
 
 void inserir(Dictionary* d, char* chave) {
-    if(d->q < d->tam && find(d, chave) == 0) {
+    if(d->q < d->tam && find(d, chave) == -1) {
         int h = hash(chave);
         int pos = h;
         int newpos;
-        if(d->tabela[pos] != NULL && d->tabela[pos].flag != 0) {
+        if(d->tabela[pos].flag != 1) {
+            Entry entrada = cria_entry(chave);
+            d->tabela[pos] = entrada;
+            d->q++;
+        }
+        else {
             int j = 0;
             do {
+                j++;
                 newpos = (h + j*j + 23*j) % 101; 
 
-            } while(d->tabela[newpos] != NULL && d->tabela[newpos].flag == 1 && j<20);
+            } while(d->tabela[newpos].flag == 1 && j<19);
+
+            if(d->tabela[newpos].flag != 1) {
+                Entry entrada = cria_entry(chave);
+                d->tabela[newpos] = entrada;
+                d->q++;
+            }
         }
+       
+        
     }
+
 }
 
 
-void remover(Dictionary* d, char* chave) {
-    if(find(d, chave) == 1) {
-        int h = hash(chave);
-        d->tabela[h].flag = 0;
-        strcpy(d->tabela[h].chave, "empty");
 
+void remover(Dictionary* d, char* chave) {
+    int h = find(d,chave);
+    if(h != -1) {
+        d->tabela[h].flag = 2;
+        strcpy(d->tabela[h].chave, "empty");
+        d->q--;
     } 
+    else {
+        
+    }
 
 }
 
 int main() {
     int test, n;
-    scanf("%d %d", &test, &n);
-    int i, j;
-    char* operacao;
-    char* chave;
+    int lol, i;
+    char string[20];
+    char *op;
+    char *chave;
 
-    Dictionary *dic = cria_dic(101);
+    scanf("%d", &test);
 
-    for(i=0;i<n;i++) {
-        for(j=0;j<4;j++) {
-            scanf(" %c", &operacao[j]);
+
+    for(lol=0;lol<test;lol++) {
+
+        scanf("%d", &n);
+        
+        Dictionary *dic = cria_dic(101);
+
+        for(i=0;i<n;i++) {
+            scanf(" %s", string);
+            op = strtok(string,":");
+            chave = strtok(NULL,":");
+            if(strcmp("ADD", op) == 0) {
+            
+              inserir(dic, chave);
+            }
+            else if(strcmp("DEL", op) == 0) {
+                remover(dic, chave);
+            }
         }
-        for(j=4;j!='\n';j++) {
-            scanf(" %c", &chave[j-4]);
+
+        printf("%d\n", dic->q);
+
+        for(i=0;i<101;i++){
+            if(dic->tabela[i].flag == 1) {
+                printf("%d:%s\n", i, dic->tabela[i].chave);
+            }
         }
-        if(strcmp("ADD:", operacao) == 0) {
-            inserir(dic, chave);
-        }
-        else if(strcmp("DEL:", operacao) == 0) {
-            remover(dic, chave);
-        }
+
+        free(dic);
+
     }
 
-    printf("%d", dic->q);
+    
 
     return 0;
 }
